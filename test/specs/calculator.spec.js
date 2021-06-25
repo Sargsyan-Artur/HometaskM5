07a3bfa7ff13e376a81aa0cb1c5603323ffa64d3
Artur <47447749+Sargsyan-Artur@users.noreply.github.com>
2021-06-25 16:55:34 +0400

    Update calculator.spec.js

--- test/specs/calculator.spec.js
+++ test/specs/calculator.spec.js
@@ -2,40 +2,36 @@ const expect = require('chai').expect;
 const calcualtor = require('../../app/calculator');
 
 describe('test calculate function', function () {
-    const calculator = new calcualtor(5,5,5,5)
+    const calculator = new calcualtor();
 
     describe('positive tests', function () {
         it('should check ADD', function () {
-            const sum = calculator.add();
+            const sum = calculator.add(5,5,5,5);
             expect(sum).to.equal(20);
         });
 
         it('should check multiply', function () {
-            const times = calculator.multiply();
+            const times = calculator.multiply(5,5,5,5);
             expect(times).to.equal(625);
         });
     });
 
     describe('negative tests', function () {
         it('space instead of value, using add function', function () {
-            const calculator = new calcualtor(   )
-            const space = calculator.add();
+            const space = calculator.add(   );
             expect(space).to.equal(0);
         });
 
         it('text instead of value, using add function ', function () {
-            const calculator = new calcualtor("a", "b")
-            expect(() => calculator.add()).to.throw(`Negative number!`);
+            expect(() => calculator.add("a", "b")).to.throw(`Negative number!`);
         });
 
         it('text instead of value, using multiply function ', function () {
-            const calculator = new calcualtor("a", "b")
-            expect(() => calculator.multiply()).to.throw(`Negative number!`);
+            expect(() => calculator.multiply("a", "b")).to.throw(`Negative number!`);
         });
 
         it('space instead of value, using multiply function', function () {
-            const calculator = new calcualtor(   )
-            const space = calculator.multiply();
+            const space = calculator.multiply(   );
             expect(space).to.equal(0);
         });
     });
